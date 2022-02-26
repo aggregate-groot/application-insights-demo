febd3ae4e01b6ef4c0f00764227cd797ced654a6
David Palumbo <david.palumbo@hotmail.com>
2022-02-26 00:30:43 -0500

    Implement Azure deployment script.

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 LOCATION=$(dirname "${BASH_SOURCE[0]}")
+BICEP_FILE=$LOCATION/../.azure/ResourceManager/Main.bicep
+RESOURCE_GROUP=Application-Insights-Demo
 
-az bicep build --file $LOCATION/../.azure/ResourceManager/Main.bicep --outdir $LOCATION/../.local/ARM/
+az bicep build --file $BICEP_FILE --outdir $LOCATION/../.local/ARM/
+
+az group create --name $RESOURCE_GROUP --location EastUS
+
+az deployment group create \
+    --resource-group $RESOURCE_GROUP \
+    --template-file $BICEP_FILE \
