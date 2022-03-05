00587147f4e56cab73b0d15f80b8d9e26803d38b
David Palumbo <david.palumbo@hotmail.com>
2022-03-05 15:23:02 -0500

    Add whisky list endpoint.

---
+++ src/Api/Controllers/WhiskiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AggregateGroot.ApplicationInsightsDemo.Api.Controllers
+{
+    /// <summary>
+    /// Manages the whisky endpoints.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhiskiesController : ControllerBase
+    {
+        /// <summary>
+        /// Lists the available whiskies.
+        /// </summary>
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok();
+        }
+    }
+}
